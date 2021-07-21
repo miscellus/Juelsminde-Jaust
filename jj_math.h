9699d03938d90b29fd706d5f2a9a167679f6d70a
Jakob Kjær-Kammersgaard <jakob@miscellus.com>
2021-07-21 03:36:11 +0200

    .

--- jj_math.h
+++ jj_math.h
@@ -1,10 +1,17 @@
 #ifndef JJ_MATH_H
 #define JJ_MATH_H
 
+#define INV_SQRT_TWO 0.7071067811865475f
+
 typedef struct Intersection_Points {
     bool are_intersecting;
     Vector2 intersection_points[2];
 } Intersection_Points;
+
+typedef struct Circle {
+    Vector2 center;
+    float radius;
+} Circle;
 
 
 float sign_float(float v);
@@ -17,6 +24,6 @@ Vector2 Vector2Abs(Vector2 v);
 
 Vector2 Vector2NormalizeOrZero(Vector2 v);
 
-Intersection_Points intersection_points_from_two_circles(Vector2 center1, float radius1, Vector2 center2, float radius2);
+Intersection_Points intersection_points_from_two_circles(Circle c1, Circle c2);
 
 #endif
