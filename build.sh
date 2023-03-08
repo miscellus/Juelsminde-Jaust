cb5c287bbf749b7c9ff2cfed3016099e83de1d98
Jakob Kjær-Kammersgaard <jkk@quasios.com>
2023-03-08 16:21:55 +0100

    .

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ if [ $machine == 'Mac' ]; then
 	cc main.c -std=c99 -Os -Wall -Wextra -pedantic -framework IOKit -framework Cocoa -framework OpenGL -I/usr/local/Cellar/raylib/3.7.0/include -L/usr/local/Cellar/raylib/3.7.0/lib -lraylib -o "$game_name"
 elif [ $machine == 'Linux' ]; then
 	# gcc -std=c99 -O0 -ggdb -Wall -Wextra -pedantic -ftabstop=1 -o "$game_name" main.c -lraylib -lGL -lm -ldl -lrt -lX11
-	gcc -std=c99 -O5 -Wall -Wextra -pedantic -ftabstop=1 -o "$game_name" main.c -lraylib -lGL -lm -ldl -lrt -lX11
+	gcc -std=c99 -O0 -ggdb -Wall -Wextra -pedantic -ftabstop=1 -o "$game_name" main.c -lraylib -lGL -lm -ldl -lrt -lX11
 fi
 
 "./$game_name"
